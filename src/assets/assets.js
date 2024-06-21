a9374e525560051bba882c1c279ce61e257d387f
Vaibhav Tupe <141541491+vaibhav0513@users.noreply.github.com>
2024-06-21 12:54:20 +0530

    add-a-donation-section

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,5 +1,14 @@
 import logo from './social.png'
+import money from './money.png'
+import growth from './growth.png'
+import teamwork from './teamwork.png'
+
 
 export const assets = {
-  logo
+  logo,
+  money,
+  growth,
+  teamwork,
+
+
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Header.css'
+import { assets } from '../../assets/assets'
 
 const Header = () => {
   return (
@@ -8,9 +9,34 @@ const Header = () => {
         <h2>Give a little. change a lot.</h2>
         <p>Help today because tomorrow you may be the one who needs helping!
         Forget what you can get and see what you can give.</p>
-        <div className="header-contents-buttons">
+        <div className="header-contents-btns">
           <button>Join with us</button>
           <button>Donate Now</button>
+        </div>
+      </div>
+
+      {/**************************************Donation Section.****************************************/}
+
+      <div className='box-container'>
+        <div className="box">
+          <img src={assets.money} alt="" />
+          <h3>Make Donation</h3>
+          <p>Help today because tomorrow you may be the one who needs helping!</p>
+          <p>Read More...</p>
+        </div>
+
+        <div className="box">
+          <img src={assets.growth} alt="" />
+          <h3>Make Donation</h3>
+          <p>Help today because tomorrow you may be the one who needs helping!</p>
+          <p>Read More...</p>
+        </div>
+
+        <div className="box">
+          <img src={assets.teamwork} alt="" />
+          <h3>Become A Member</h3>
+          <p>Help today because tomorrow you may be the one who needs helping!</p>
+          <p>Read More...</p>
         </div>
       </div>
     </div>
