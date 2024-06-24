666e2088efbaa2b2c5cce2f639a805429bdef4c8
Sohamsadvilkar <sohamsadvilkar98@gmail.com>
2024-06-24 23:32:07 +0530

    Make Changes

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,12 +2,9 @@ import logo from './social.png'
 import money from './money.png'
 import growth from './growth.png'
 import teamwork from './teamwork.png'
-<<<<<<< HEAD
 import history from './history.jpg'
 import mission from './mission.jpg'
 import profile  from './profile.jpg'
-=======
->>>>>>> eba2402f4273292067ca4e0033e43d5f7770f7e7
 import director from './director.png'
 import teamMemberOne from './teamMemberOne.png'
 import teamMemberTwo from './teamMemberTwo.png'
@@ -20,15 +17,12 @@ export const assets = {
   money,
   growth,
   teamwork,
-<<<<<<< HEAD
   history,
   mission,
   profile,
 
 
 
-=======
->>>>>>> eba2402f4273292067ca4e0033e43d5f7770f7e7
   director,
   teamMemberOne,
   teamMemberTwo,
