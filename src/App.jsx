dbe38b3a4d5752e459106deebf00a22d26fa7ed1
Sohamsadvilkar <sohamsadvilkar98@gmail.com>
2024-06-24 18:44:56 +0530

    Latest Pull

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import Blogs from './pages/Blogs/Blogs'
-
+//import Goals from './components/Goals/Goals';
 
 const App = () => {
   return (
@@ -14,6 +14,7 @@ const App = () => {
          <Route path='/' element={<Home/>} />
          <Route path='/Blogs' element={<Blogs/>} />
          <Route path='/About' element={<About/>} />
+         {/* <Route path='/Goals' element={<Goals/>} /> */}
       </Routes>
       <Home/>
     </div>
--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,13 +2,18 @@ import logo from './social.png'
 import money from './money.png'
 import growth from './growth.png'
 import teamwork from './teamwork.png'
-
-
+import history from './history.jpg'
+import mission from './mission.jpg'
+import profile  from './profile.jpg'
 export const assets = {
   logo,
   money,
   growth,
   teamwork,
+  history,
+  mission,
+  profile,
+
 
 
 }
