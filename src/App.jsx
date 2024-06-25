ef1fcb06344a60eb276326c5b41e38dfd3e34c91
Sohamsadvilkar <sohamsadvilkar98@gmail.com>
2024-06-25 15:15:05 +0530

    Make Changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,10 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import Blogs from './pages/Blogs/Blogs'
-<<<<<<< HEAD
 import Goals from './components/Goals/Goals'
 import TeamMember from './components/TeamMember/TeamMember'
 import Title from './components/Title/Title'
-//import TeamMember from './components/TeamMember/TeamMember'
-//import Title from './components/Title/Title'
-=======
-import TeamMember from './components/TeamMember/TeamMember'
-import Title from './components/Title/Title'
->>>>>>> 0803f5be2c459a7914cc4d0000ef60db5959e970
+
 
 
 const App = () => {
@@ -26,7 +20,7 @@ const App = () => {
          <Route path='/About' element={<About/>} />
       </Routes>
       <Home/>
-
+      <Goals/>
       <div className="container">
         <Title title='Meet Our Members' subtitle='Help today because tomorrow you may be the one who needs more helping!'/>
          <TeamMember/>
