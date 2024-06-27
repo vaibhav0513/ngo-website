d9eb0618e902ed2274fc4d40da1506102a300763
Vaibhav Tupe <141541491+vaibhav0513@users.noreply.github.com>
2024-06-27 18:13:50 +0530

    feature-contact-section & update-header-section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,44 +4,15 @@ import { assets } from '../../assets/assets'
 
 const Header = () => {
   return (
-    <div className='header'>
-      <div className="header-contents">
-        <h2>Give a little. change a lot.</h2>
-        <h4>Together we can make a Difference</h4>
-        <p>Help today because tomorrow you may be the one who needs helping!
-        Forget what you can get and see what you can give.Help today because tomorrow you may be the one who needs helping!
-        Forget what you can get and see what you can give.Help today because tomorrow you may be the one who needs helping!
-        Forget what you can get and see what you can give.</p>
-        <div className="header-contents-btns">
-          <button>Join with us</button>
-          <button>Donate Now</button>
-        </div>
+    <div className='header container'>
+      <div className="header-text">
+        <h1>Transforming Communities with Love and Support</h1>
+        <p>Welcome to our organization, where compassion meets action. We are dedicated to providing essential support to those in need, ensuring that everyone in our community has access to the basic necessities of life.</p>
+        <button className='btn'>Join Our Mission <img src={assets.dark_arrow} alt="" /></button>
       </div>
+     
 
-      {/**************************************Donation Section.****************************************/}
-
-      {/* <div className='box-container'>
-        <div className="box">
-          <img src={assets.money} alt="" />
-          <h3>Make Donation</h3>
-          <p>Help today because tomorrow you may be the one who needs helping!</p>
-          <p>Read More...</p>
-        </div>
-
-        <div className="box">
-          <img src={assets.growth} alt="" />
-          <h3>Fundrising</h3>
-          <p>Help today because tomorrow you may be the one who needs helping!</p>
-          <p>Read More...</p>
-        </div>
-
-        <div className="box">
-          <img src={assets.teamwork} alt="" />
-          <h3>Become A Member</h3>
-          <p>Help today because tomorrow you may be the one who needs helping!</p>
-          <p>Read More...</p>
-        </div>
-      </div> */}
+     
     </div>
   )
 }
---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import './Contact.css'
+import { assets } from '../../assets/assets'
+
+const Contact = () => {
+    const [result, setResult] = React.useState("");
+  
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      setResult("Sending message.");
+      const formData = new FormData(event.target);
+  
+      formData.append("access_key", "0e1494d2-6fd1-4152-bace-422f1fafcbd4");
+  
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      });
+  
+      const data = await response.json();
+  
+      if (data.success) {
+        setResult("Form Submitted Successfully...😊");
+        event.target.reset();
+      } else {
+        console.log("Error", data);
+        setResult(data.message);
+      }
+    };
+
+  return (
+    <div className='contact'>
+      <div className="contact-col">
+        <h3>Send us a message <img src={assets.msg_icon} alt="" /></h3>
+        <p>Feel free to reach out through contact form or find our contact information below. Your feedback, questions, and suggestions are important to us as we strive to provide exceptional service to our NGO community.</p>
+        <ul>
+          <li><img src={assets.mail_icon} alt="" /> Contact@RudraVeer.in</li>
+          <li><img src={assets.phone_icon} alt="" /> +91 9876543210</li>
+          <li><img src={assets.location_icon} alt="" /> 102-A,Galaxy Apt. Kolshet, MH, India</li>
+        </ul>
+      </div>
+      <div className="contact-col">
+        <form onSubmit={onSubmit}>
+          <label>Your name</label>
+          <input type="text" name='name' placeholder='Enter your name' required />
+
+          <label>Phone Number</label>
+          <input type="text" name='phone' placeholder='Enter your mobile number' required />
+
+          <label>Write your message here</label>
+          <textarea name="message" rows="6" placeholder='Enter your message' required></textarea>
+
+          <button type='submit' className='btn dark-btn'>Send Message</button>
+        </form>
+        <span>{result}</span>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Contact
